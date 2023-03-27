c718fc56e3575b64e9ec044fcdcf4139ac2685e2
Lâm Nhật Minh <minh32405@gmail.com>
2023-03-27 21:28:54 +0700

    Cật nhật lại nhánh main- Lâm Nhật Minh

---
+++ ThucHangGitHub/DAL/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThucHangGitHub.DTO;
+namespace ThucHangGitHub.DAL
+{
+    class Class1
+    {
+
+    }
+}
---
+++ ThucHangGitHub/GUI/MainForm.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ThucHangGitHub.BLL;
+using ThucHangGitHub.DTO;
+namespace ThucHangGitHub.GUI
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
